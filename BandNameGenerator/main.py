5ad7cd734ea0eede70507a5f8450dac26f43d115
Mykhailo Oleksyn <mykhailo.oleksyn74@gmail.com>
2023-11-18 18:49:44 +0200

    Added BandNameGenerator

---
+++ BandNameGenerator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator")
+
+city_name = input("What is the city name you grew up in?\n")
+
+pet_name = input("What is the name of a pet you had as a kid (Or currently have)?\n")
+
+print("The best naming for your band based on the input data is '" + city_name + " " + pet_name + "'")
